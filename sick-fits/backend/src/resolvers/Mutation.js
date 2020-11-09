91796aafdedff7544525d198fd626c650e2ca460
Zachary Guerrin <z.guerrin@gmail.com>
2020-11-09 08:59:23 -0500

    Finishing project for now. Not relavant to learning path

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,23 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    // create dog 
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutations.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    
+    return item;
   }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   // create dog 
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = Mutations;
